fcd5472e98792df602e4f042680b83d7c476add0
Jamar Gibbs <jg_220@yahoo.com>
2023-02-28 13:24:42 -0800

    add tests

--- Tests/AIImageSwiftTests/AIImageSwiftTests.swift
+++ Tests/AIImageSwiftTests/AIImageSwiftTests.swift
@@ -2,10 +2,13 @@ import XCTest
 @testable import AIImageSwift
 
 final class AIImageSwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AIImageSwift().text, "Hello, World!")
+    func testSaveImage() {
+        let image = UIImage(named: "testImage")!
+        let imageGenerator = SwiftAIImage(config: Configuration(apiKey: "apikey", modelID: "modelid"))
+        imageGenerator.saveImage(image: image, name: "testImage")
+    
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let filePath = "\(documentsPath)/testImage.jpeg"
+        XCTAssertTrue(FileManager.default.fileExists(atPath: filePath))
     }
 }
